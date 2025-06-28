4b109d432728c7f16eab6b710fb5769f3e0e3738
Air2air <todd@dunningmarketing.com>
2025-06-28 13:52:26 -0700

    Prompts to Git

---
+++ app/components/json-ld.tsx
@@ -0,0 +1,87 @@
+import React from 'react'
+
+interface JsonLdProps {
+  data: Record<string, any>
+}
+
+export function JsonLd({ data }: JsonLdProps) {
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+    />
+  )
+}
+
+// Predefined schema generators
+export const schemas = {
+  person: (data: {
+    name: string
+    description?: string
+    url?: string
+    sameAs?: string[]
+    jobTitle?: string
+    worksFor?: string
+  }) => ({
+    '@context': 'https://schema.org',
+    '@type': 'Person',
+    ...data
+  }),
+
+  blogPosting: (data: {
+    headline: string
+    description: string
+    author: string
+    datePublished: string
+    dateModified?: string
+    url: string
+    image?: string
+  }) => ({
+    '@context': 'https://schema.org',
+    '@type': 'BlogPosting',
+    headline: data.headline,
+    description: data.description,
+    author: {
+      '@type': 'Person',
+      name: data.author
+    },
+    datePublished: data.datePublished,
+    dateModified: data.dateModified || data.datePublished,
+    url: data.url,
+    mainEntityOfPage: {
+      '@type': 'WebPage',
+      '@id': data.url
+    },
+    ...(data.image && { image: data.image })
+  }),
+
+  website: (data: {
+    name: string
+    description: string
+    url: string
+    author?: string
+  }) => ({
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    name: data.name,
+    description: data.description,
+    url: data.url,
+    ...(data.author && {
+      author: {
+        '@type': 'Person',
+        name: data.author
+      }
+    })
+  }),
+
+  breadcrumbList: (items: Array<{ name: string; url: string }>) => ({
+    '@context': 'https://schema.org',
+    '@type': 'BreadcrumbList',
+    itemListElement: items.map((item, index) => ({
+      '@type': 'ListItem',
+      position: index + 1,
+      name: item.name,
+      item: item.url
+    }))
+  })
+}
