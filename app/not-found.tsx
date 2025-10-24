083dd44ccb3042e44553ef9fc666cb3f8976cc7e
Air2air <todd@dunningmarketing.com>
2025-10-23 18:09:32 -0700

    Redirect 404 errors to home page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,17 +1,5 @@
-import { SITE_CONFIG } from './utils/constants';
-
-export const metadata = {
-  title: `404 - Page Not Found | ${SITE_CONFIG.shortName}`,
-  description: 'The page you are looking for does not exist.'
-};
+import { redirect } from 'next/navigation';
 
 export default function NotFound() {
-  return (
-    <section>
-      <h1 className="mb-8 tracking-tight">
-        404 - Page Not Found
-      </h1>
-      <p className="mb-4">The page you are looking for does not exist.</p>
-    </section>
-  )
+  redirect('/');
 }
