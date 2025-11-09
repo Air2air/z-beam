586e524d2e180e84162285967f67012cabd92ee5
Air2air <todd@dunningmarketing.com>
2025-11-09 00:19:00 -0800

    Fix: TypeScript export for CaptionContent default export

--- app/components/Caption/index.ts
+++ app/components/Caption/index.ts
@@ -2,7 +2,7 @@
 export { Caption } from './Caption';
 export { CaptionHeader } from './CaptionHeader';
 export { CaptionImage } from './CaptionImage';
-export { CaptionContent } from './CaptionContent';
+export { default as CaptionContent } from './CaptionContent';
 export { TechnicalDetails } from './TechnicalDetails';
 export { MetadataDisplay } from './MetadataDisplay';
 export { useCaptionParsing } from './useCaptionParsing';
